b37d47a3ea549155026d9a04f6ae232d032d9d04
KAishwaryaa <aishwaryaa2k4@gmail.com>
2026-05-18 07:48:56 +0530

    fix cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://resonant-marshmallow-f44195.netlify.app"
+    "https://fastidious-bombolone-704ca1.netlify.app"
 ]
 
     model_config = {
